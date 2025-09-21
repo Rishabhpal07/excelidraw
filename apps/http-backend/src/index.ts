6eea31effc3b092d2e06358a24b7065463e75267
Rishabhpal07 <rishabhpal21390@gmail.com>
2025-09-21 10:38:08 +0530

    added  db

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,40 @@
 import express from "express"
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "@repo/backend-common/config";
+//@ts-ignore
+import CreateUserSchema from "@repo/common/types";
 
 const app=express();
+
+app.post("signin",(req,res)=>{
+
+    const data=CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"incorrect data"
+        })
+    }
+    
+
+
+})
+
+app.post("signup",(req,res)=>{
+
+    const userId=1;
+    const token=jwt.sign({
+        userId
+        //@ts-ignore
+    },JWT_SECRET)
+
+    res.json(token)
+    
+})
+
+app.post("room",(req,res)=>{
+    
+})
+
+
 
 app.listen(3001);
